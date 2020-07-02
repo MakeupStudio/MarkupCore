b7ba3e7cc380c158ef526db4a3b2d7ecfc29c936
maximkrouk <id.maximkrouk@gmail.com>
2020-07-03 01:13:57 +0300

    A few document content formats added

--- Sources/MarkupCore/Document/Protocols/DocumentContent.swift
+++ Sources/MarkupCore/Document/Protocols/DocumentContent.swift
@@ -6,7 +6,30 @@
 //  Copyright © 2019 MakeupStudio. All rights reserved.
 //
 
+import Foundation
+
 public protocol DocumentContent {
     associatedtype Body
     var body: Body { get }
+}
+
+public struct MarkupDocumentContent: DocumentContent {
+    public var body: Node
+    public init(_ body: Node) {
+        self.body = body
+    }
+}
+
+public struct StringDocumentContent: DocumentContent {
+    public var body: String
+    public init(_ body: String) {
+        self.body = body
+    }
+}
+
+public struct DataDocumentContent: DocumentContent {
+    public var body: Data
+    public init(_ body: Data) {
+        self.body = body
+    }
 }
